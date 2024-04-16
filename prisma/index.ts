250b423cb66354b58dd65ac93e47d0e9796f1dd3
Kareem Amara <kareemamarah2356@gmail.com>
2024-04-16 22:02:19 +0200

    adding prisma schema and .env file and linking to the mongodb compass

---
+++ prisma/index.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: "prisma@prisma.com",
+      name: "Codeawy",
+    },
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
