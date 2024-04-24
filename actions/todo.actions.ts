0837ec5a4fc226557e0c8224a2008992c533c16a
Kareem Amara <kareemamarah2356@gmail.com>
2024-04-24 17:56:40 +0200

    create a new todo

---
+++ actions/todo.actions.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getTodoListAction = async () => {
+  //   Error Handling
+  return await prisma.todo.findMany();
+};
+export const createTodoAction = async ({
+  title,
+  body,
+}: {
+  title: string;
+  body?: string | undefined;
+}) => {
+  await prisma.todo.create({
+    data: {
+      title,
+      body,
+    },
+  });
+};
+export const updateTodoAction = async () => {};
+export const deleteTodoAction = async () => {};
---
+++ components/AddTodoForm.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+import { TodoFormValues, todoFormSchema } from "@/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { createTodoAction } from "@/actions/todo.actions";
+const AddTodoForm = () => {
+  const defaultValues: Partial<TodoFormValues> = {
+    title: "DEFAULT TITLE",
+    body: "DEFAULT BODY",
+  };
+  //** getTodoListAction */
+  // const todos = await getTodoListAction();
+
+  const form = useForm<TodoFormValues>({
+    resolver: zodResolver(todoFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data: TodoFormValues) => {
+    console.log(data);
+    await createTodoAction({ title: data.title, body: data.body });
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus size={14} className="mr-1" />
+          New Todo
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="py-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Go to gym" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="body"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Short Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Tell us a little bit about yourself"
+                        className="resize-none"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      You can write a short description about your next todo .
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Save changes</Button>
+            </form>
+          </Form>
+        </div>
+        <DialogFooter></DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddTodoForm;
